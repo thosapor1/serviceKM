5db0350f491eaa4226292a9118fd4f614b265e20
Pu <thosapor@hotmail.com>
2022-03-24 16:58:33 +0700

    connect to sql server

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = new express();
+const sql = require("mssql");
 
-app.get("/", (req, res) => {
-  res.send("hello, express");
+const config = {
+  user: "thosapornc",
+  password: "thosapornc",
+  server: "192.168.10.15",
+  database: "EARTH3",
+  options: {
+    encrypt: false,
+    enableArithAbort: true,
+  },
+};
+
+sql.connect(config, (err) => {
+  if (err) console.log(err);
+  const request = new sql.Request();
+  const query = `select * from dev_test`;
+  request.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 const PORT = process.env.port || 5000;
